562ef909a440b93ca6ba0fa23f5fd311b053fe50
sohamjaiswal10 <sohamjaiswal1@gmail.com>
2020-02-18 13:03:19 +0530

    Delete broadcast.sh

--- broadcast.sh
+++
@@ -1,6 +0,0 @@
-#The broadcast script can be placed at the desktop 
-#and can access normal mp3 files from the music folder,
-#sox will convert the mp3 to wav and pipe it to pi_fm_rds.
-#Just makes the whole thing more accessible.
-#Only drawback is that sox will become a prerequsite
-sox -t mp3 ~/Music/*mp3 -t wav - | sudo ./pi_fm_rds -audio -
